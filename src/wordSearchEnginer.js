0cd55f6679c17b841f0195965cf6d5774d5f1608
Jibin <jibinjj@yahoo.ca>
2021-05-17 22:59:48 -0400

    get chars from matx

--- src/wordSearchEnginer.js
+++ src/wordSearchEnginer.js
@@ -1,5 +1,33 @@
-function wordSearchEngine(){
-    console.log("hi there")
+import { matrix, words } from "./halloween";
+import { _ } from "lodash";
+
+function wordSearchEngine(matxChars, lstWords) {
+  let horiz = extractHorizChars(matxChars);
+  let vert = extractVertChars(matxChars);
+  console.log()
+  console.log(vert.down)
 }
 
-wordSearchEngine()
+function extractHorizChars(matxChars) {
+  let horizR = "";
+  let horizL = "";
+  for (let row of matxChars) {
+    horizR += row.join("") + "~";
+    horizL += row.reverse().join("") + "~";
+  }
+  return { right: horizR, left: horizL };
+}
+
+function extractVertChars(matxChars) {
+  let transposedMatx = _.zip(...matxChars);
+  let vertU = "";
+  let vertD = "";
+
+  for (let row of transposedMatx) {
+    vertD += row.join("") + "~";
+    vertU += row.reverse().join("") + "~";
+  }
+  return { up: vertU, down: vertD };
+}
+
+wordSearchEngine(matrix, words);
